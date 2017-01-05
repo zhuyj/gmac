c854049f5f88634453851fdf820107dabe0bfc2a
Zhu Yanjun <yanjun.zhu@oracle.com>
2017-01-05 02:16:53 -0500

    auto.sh: add .config file at beginning

--- auto.sh
+++ auto.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+if [ -f config-txvh ]; then
+	if [ ! -f .config ]; then
+		cp -f config-txvh .config
+		make oldconfig
+	fi
+fi
 make drivers/net/ethernet/txvh/txvh_gmac.ko
 #rmmod txvh_gmac
