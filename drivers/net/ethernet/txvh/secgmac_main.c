63c59ec5b04539e3e5d6bdeab3fa25026bf399ca
wind <zyjzyj2000@gmail.com>
2017-05-26 18:02:29 +0800

    secgmac: write the first byte to 0x5FB

--- drivers/net/ethernet/txvh/secgmac_main.c
+++ drivers/net/ethernet/txvh/secgmac_main.c
@@ -7055,7 +7055,13 @@ static netdev_tx_t secgmac_start_xmit(struct sk_buff *skb,
 
 	memset_io(PCIE_RX_BUF + PCIE_RX_BUF_LEN * entry, 0, PCIE_RX_BUF_LEN);
 	memcpy_toio(PCIE_RX_BUF + PCIE_RX_BUF_LEN * entry, skb->data, skb->len);
-	secgmac_debug("The first 4 bytes:0x%4x", readl(PCIE_RX_BUF + PCIE_RX_BUF_LEN * entry));
+	writeb(skb->data[0], PCIE_RX_BUF + PCIE_RX_BUF_LEN * entry + 0x5FB);
+	secgmac_debug("0-3 bytes:0x%4x, 4-7 bytes:0x%4x,8-11 bytes:0x%4x,12-15 bytes:0x%4x, 0x5FB:0x%x",
+		      readl(PCIE_RX_BUF + PCIE_RX_BUF_LEN * entry),
+		      readl(4+PCIE_RX_BUF + PCIE_RX_BUF_LEN * entry),
+		      readl(8+PCIE_RX_BUF + PCIE_RX_BUF_LEN * entry),
+		      readl(12+PCIE_RX_BUF + PCIE_RX_BUF_LEN * entry),
+		      readb(0x5FB+PCIE_RX_BUF + PCIE_RX_BUF_LEN * entry));
 	writel(skb->len, PCIE_RX_BUF + PCIE_RX_BUF_LEN * entry + 0x5FC);//skb length
 	spin_unlock(&tp->lock);
 	tp->cur_tx = entry + 1;
